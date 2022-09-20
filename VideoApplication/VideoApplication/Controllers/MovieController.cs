9eca3bbb8524ab7d356e901997778ed5cde2cc4d
Khokila V <khokila.viswanaathan@gmail.com>
2022-09-20 09:44:00 +0200

    video

---
+++ VideoApplication/VideoApplication/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoApplication.Models;
+
+namespace VideoApplication.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Id = 001,Name = "James Bond 007"};
+            return View(movie);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id ="+ id);
+        }
+    }
+}
---
+++ VideoApplication/VideoApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideoApplication.MvcApplication" Language="C#" %>
